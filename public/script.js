c6a63593a29f6ea3e0f6af44492d3ece1c2635e1
Ryan Chen <RyanChen54321@gmail.com>
2021-07-23 18:06:50 +0000

    Added functionality to the database

--- public/script.js
+++ public/script.js
@@ -1,3 +1,21 @@
-var user = new Peer(); 
+var peer = new Peer();  
 
-console.log(user); 
+peer.on('open', function(id) {
+  var Peerid = id;
+});
+
+function joinCall() {
+    // var call = peer.call('id here', mediaStream);
+        
+
+}
+
+
+// console.log(peer._id); 
+
+function createCall() {
+     firebase.database().ref("/inviteCodes").push({
+        peerID : peer.id 
+        // inviteCode : peer
+    });
+}
