9596970c75c6b86184ff1e4af77bcef96f4abfa8
Sebastian Ingino <sebasing@icloud.com>
2021-07-23 12:53:25 -0500

    Added model support

---
+++ public/modal.js
@@ -0,0 +1,9 @@
+modal = document.getElementById('modal');
+
+const joinCall = () => {
+    modal.classList.add('is-active');
+};
+
+const hideModal = () => {
+    modal.classList.remove('is-active');
+};
